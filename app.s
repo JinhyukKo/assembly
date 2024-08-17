c7f6efe71969bbfaf14909886ffc0350b80da957
Jinhyuk <kobin1970@gmail.com>
2024-08-17 10:23:47 +0200

    revised

--- app.s
+++ app.s
@@ -1,13 +1,20 @@
-.data
-msg: .asciz "Hello, World!\n"
+.global _start             // Provide program starting address to linker
+.align 2
 
-.text
-.global _start
+// Setup the parameters to print hello world
+// and then call Linux to do it.
 
-_start:
-    ldr x0, =msg      ; Load the address of msg into x0
-    bl  printf        ; Call printf to print the message
-    mov x0, #0        ; Return code 0
-    mov x8, #93       ; Exit syscall number
-    svc #0            ; Make syscall to exit
+_start: mov X0, #1     // 1 = StdOut
+        adr X1, helloworld // string to print
+        mov X2, #13     // length of our string
+        mov X16, #4     // MacOS write system call
+        svc 0     // Call linux to output the string
 
+// Setup the parameters to exit the program
+// and then call Linux to do it.
+
+        mov     X0, #0      // Use 0 return code
+        mov     X16, #1     // Service command code 1 terminates this program
+        svc     0           // Call MacOS to terminate the program
+
+helloworld:      .ascii  "Hello World!\n"
