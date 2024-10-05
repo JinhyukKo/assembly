f62038056a3585a42817a30250758b55f8a91d02
Jinhyuk <kobin1970@gmail.com>
2024-10-05 17:08:37 +0200

    merge

--- arith/arith.s
+++ arith/arith.s
@@ -1,12 +1,13 @@
 .global _start             // Provide program starting address to linker
 .align 2
 
+output_string:      .asciz  "the result of 1+1 is :\n"
+
 // Setup the parameters to print hello world
 // and then call Linux to do it.
-
 _start: mov X0, #1     // 1 = StdOut
-        adr X1, helloworld // string to print
-        mov X2, #13     // length of our string
+        adr X1, output_string // string to print
+        mov X2, #23   // length of our string
         mov X16, #4     // MacOS write system call
         svc 0     // Call linux to output the string
 
@@ -17,4 +18,4 @@ _start: mov X0, #1     // 1 = StdOut
         mov     X16, #1     // Service command code 1 terminates this program
         svc     0           // Call MacOS to terminate the program
 
-helloworld:      .ascii  "Hello World!\n"
+
